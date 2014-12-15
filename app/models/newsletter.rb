14b975ad25e075802c665b7b3ff6bd4d95436981
Joao Aguilera <jg.alg34@gmail.com>
2014-12-14 22:43:33 -0800

    added css and html files

--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -1,2 +1,6 @@
 class Newsletter < ActiveRecord::Base
+
+  validates_presence_of :name
+  validates_format_of :email, :with =>  /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :message => "Please enter a valid email address"
+
 end
